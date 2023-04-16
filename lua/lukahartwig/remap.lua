583f2824b40cbbd64a7f36d63bb4f65fda883177
Luka Hartwig <mail@lukahartwig.de>
2023-04-16 15:04:10 +0200

    Add remappings for quickfix list

--- lua/lukahartwig/remap.lua
+++ lua/lukahartwig/remap.lua
@@ -3,3 +3,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>cn", "<Cmd>cnext<CR>")
+vim.keymap.set("n", "<leader>cp", "<Cmd>cprev<CR>")
+vim.keymap.set("n", "<leader>cq", "<Cmd>cclose<CR>")
+
